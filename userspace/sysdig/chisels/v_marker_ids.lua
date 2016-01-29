71d3072e303ba1cfdfcdce12dcc809a55ec836f3
Loris Degioanni <loris@sysdig.com>
2016-01-28 17:05:17 -0800

    more marker views

---
+++ userspace/sysdig/chisels/v_marker_ids.lua
@@ -0,0 +1,81 @@
+--[[
+Copyright (C) 2013-2015 Draios inc.
+ 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 2 as
+published by the Free Software Foundation.
+
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--]]
+
+view_info = 
+{
+	id = "marker_ids",
+	name = "01Markers",
+	description = "Show the top system calls in the system based on number of invocations and time spent calling them.",
+	tags = {"Default"},
+	view_type = "table",
+	applies_to = {"", "container.id", "proc.pid", "proc.name", "thread.tid", "fd.directory", "evt.res", "k8s.pod.id", "k8s.rc.id", "k8s.svc.id", "k8s.ns.id"},
+--	use_defaults = true,
+--	filter = "evt.type=marker",
+	filter = "marker.ntags>=%depth",
+	drilldown_target = "markers",
+	spectro_type = "markers",
+	drilldown_increase_depth = true,
+	columns = 
+	{
+		{
+			name = "NA",
+--			field = "marker.tag[%depth]",
+			field = "marker.id",
+			is_key = true
+		},
+		{
+			name = "ID",
+			field = "marker.id",
+		},
+		{
+			is_sorting = true,
+			name = "#HITS",
+			field = "marker.count.fortag[%depth]",
+			description = "Number of calls per second for this system call.",
+			colsize = 10,
+			aggregation = "SUM"
+		},
+		{
+			name = "AVG TIME",
+			field = "marker.latency",
+			description = "average time this marker took to complete",
+			colsize = 10,
+			aggregation = "AVG"
+		},
+		{
+			name = "MIN TIME",
+			field = "marker.latency",
+			description = "minimum time this marker took to complete",
+			colsize = 10,
+			aggregation = "MIN"
+		},
+		{
+			name = "MAX TIME",
+			field = "marker.latency",
+			description = "maximum time this marker took to complete",
+			colsize = 10,
+			aggregation = "MAX"
+		},
+		{
+			name = "TAG",
+			field = "marker.tag[%depth]",
+			description = "Marker tag.",
+			colsize = 32,
+			aggregation = "SUM"
+		},
+	}
+}
---
+++ userspace/sysdig/chisels/v_spectro_markers.lua
@@ -0,0 +1,48 @@
+--[[
+Copyright (C) 2013-2015 Draios inc.
+ 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 2 as
+published by the Free Software Foundation.
+
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--]]
+view_info = 
+{
+	id = "spectro_app",
+	name = "Spectrogram-App",
+	description = "Application markers latency spectrogram.",
+	view_type = "spectrogram",
+	applies_to = {"", "container.id", "proc.pid", "thread.tid", "proc.name", "evt.res", "k8s.pod.id", "k8s.rc.id", "k8s.svc.id", "k8s.ns.id", "fd.name", "fd.containername", "fd.directory", "fd.containerdirectory"},
+	filter = "marker.ntags=%depth",
+	use_defaults = false,
+--	drilldown_target = "markers",
+	drilldown_target = "dig_app",
+	columns = 
+	{
+		{
+			name = "NA",
+			field = "marker.latency.quantized",
+			is_key = true
+		},
+		{
+			name = "LATENCY",
+			description = "system call latency.",
+			field = "marker.latency.quantized",
+		},
+		{
+			name = "COUNT",
+			description = "Number of markers.",
+			field = "evt.count",
+			aggregation = "SUM",
+			colsize = 8,
+		}
+	}
+}
